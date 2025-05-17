1f21176772284701896c3a9eadc709beb29c08d7
zxcmeowzxc <Shmakova2007@outlook.com>
2025-05-17 16:55:56 +0300

    6

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+
+const roundCount = 3;
+
+export default (generateRound, description) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(description);
+
+  for (let i = 0; i < roundCount; i += 1) {
+    const [question, correctAnswer] = generateRound();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ').trim();
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
---
+++ src/utils/random.js
@@ -0,0 +1,2 @@
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+export default getRandomNumber;
