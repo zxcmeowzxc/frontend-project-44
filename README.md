ba32d6dfea08565d1a83cfa4264a4c28638973c1
zxcmeowzxc <Shmakova2007@outlook.com>
2025-05-18 12:38:08 +0300

    8

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [![asciicast](https://asciinema.org/a/gE1BUEcWnPiuBkzVM0aUEv7AA.svg)](https://asciinema.org/a/gE1BUEcWnPiuBkzVM0aUEv7AA)
 [![asciicast](https://asciinema.org/a/WRAOJaQsJjR9zOjYmg7nulRSb.svg)](https://asciinema.org/a/WRAOJaQsJjR9zOjYmg7nulRSb)
 [![asciicast](https://asciinema.org/a/GdWsES2hL6RXZFtBfmzSH9zau.svg)](https://asciinema.org/a/GdWsES2hL6RXZFtBfmzSH9zau)
+[![asciicast](https://asciinema.org/a/c7SKgA7OgmE37h0g7WN1O9MnI.svg)](https://asciinema.org/a/c7SKgA7OgmE37h0g7WN1O9MnI)
---
+++ src/games/progression.js
@@ -0,0 +1,24 @@
+import runGame from '../index.js';
+import getRandomNumber from '../utils/random.js';
+
+const description = 'What number is missing in the progression?';
+
+const generateRound = () => {
+  const start = getRandomNumber(1, 20);
+  const step = getRandomNumber(2, 10);
+  const length = 10;
+
+  const progression = [];
+  for (let i = 0; i < length; i++) {
+    progression.push(start + i * step);
+  }
+
+  const hiddenIndex = getRandomNumber(0, length - 1);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+
+  const question = progression.join(' ');
+  return [question, correctAnswer];
+};
+
+export default () => runGame(description, generateRound);
